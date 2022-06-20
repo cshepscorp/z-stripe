e9c16c259b48b8b4b398bd033dc0db25fce2c7f3
Christina Sheppard <36707189+cshepscorp@users.noreply.github.com>
2022-06-20 14:35:28 -0400

    hero set up

--- src/Hero.js
+++ src/Hero.js
@@ -2,9 +2,28 @@ import React from 'react';
 import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments ingrastructure</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
+            officiis doloribus enim error iure beatae, placeat, dolorem alias
+            quaerat in modi, corrupti animi exercitationem repellendus quae
+            possimus voluptates quod debitis saepe? Voluptate harum quaerat,
+            quia, doloribus recusandae repudiandae ipsum animi quidem tenetur,
+            quibusdam sapiente odit?
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className="hero-images">
+          <img alt="phone-img" src={phoneImg} />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
